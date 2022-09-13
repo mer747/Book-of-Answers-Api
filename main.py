59f6700c6898df8d74da3732cdeaf4853fa5de89
mer747 <mertposta07@gmail.com>
2022-09-13 21:47:41 +0300

    Appearance Improvements

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 
-import generator.yes as yesAnswer
-import generator.no as noAnswer
-import generator.random as randomAnswer
+from generator.yes import Yes
+from generator.no import No
+from generator.random import Random
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(noAnswer.No, "/no")
-api.add_resource(yesAnswer.Yes, "/yes")
-api.add_resource(randomAnswer.Random, "/random")
+api.add_resource(No, "/no")
+api.add_resource(Yes, "/yes")
+api.add_resource(Random, "/random")
 
 if __name__ == '__main__':
     app.run(debug=False)
